55e8c22e9288c0441a05572f6a08a451ec0104e2
kiran kashif azad <kirankashifazad@gmail.com>
2024-03-02 01:00:37 +0500

    work is done

---
+++ app.js
@@ -0,0 +1,71 @@
+var a=("<h1>array assigment</h1>")
+document.write(a)
+
+
+var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+document.write(arr ,"<br>")
+var b=arr.slice (2,4)
+ document.write(b +"<br><br>")
+
+
+ var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+ document.write(arr ,"<br>")
+ var b=arr.splice (2,4)
+  document.write(b +"<br><br>")
+
+  var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+ document.write(arr ,"<br>")
+ var b=arr.shift()
+  document.write(b +"<br><br>")
+
+
+  var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+  document.write(arr ,"<br>")
+  arr.unshift("huma")
+ document.write(arr,"<br><br>")
+
+ var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+ document.write(arr ,"<br>")
+ arr.push("abid")
+document.write(arr,"<br><br>")
+
+
+var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+ document.write(arr ,"<br>")
+ var b=arr.pop ()
+  document.write(b +"<br><br>")
+
+
+  var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+document.write(arr ,"<br>")
+var b=arr.slice (1,4)
+ document.write(b +"<br><br>")
+
+
+ var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+ document.write(arr ,"<br>")
+ var b=arr.splice (0,2)
+  document.write(b +"<br> <br>")
+
+
+  var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+  document.write(arr ,"<br>")
+  var b=arr.shift (0)
+  document.write(b +"<br>")
+  var c=arr.shift (1)
+  document.write(c +"<br>")
+  var d=arr.shift (2)
+  document.write(d +"<br>")
+  var e=arr.shift (3)
+  document.write(e +"<br>")
+  var f=arr.shift (4)
+  document.write(f+"<br><br>")
+
+
+  var arr=[ "ali" ,"sehar","soha","hiba","dani"]
+  document.write(arr,"<br>")
+ arr.push ("kiran","kaif","roha")
+  document.write(arr +"<br> <br>")
+
+
+
